c75336ee661825a0f2dba3aa0e6cac59a56b2bc5
Romario <yohanes.romario@dealpos.com>
2024-07-21 18:22:56 +0700

    add navbar for website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,10 @@ import { AskAIButtonSC } from "./AskAIButton";
 import { Flex } from "./FlexCointainer";
 import "./Navbar.css"
 import Header from "./Header";
+import AIQueryButtonContainer from "./AIQuery";
+import ConfigurationButtonContainer from "./Config";
+import AddInvestmentContainer from "./AddInvestment";
+import { SectionTitle } from "./SectionHeading";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -23,18 +27,24 @@ export default function Navbar() {
                 <Flex $justifyContent="space-between" $alignItems="center" className="my-5">
                     <Flex>
                         <img src="/avatar.svg"></img>
-                        <img src="/brand.svg"></img>
                     </Flex>
-                    <div style={{ flexGrow: 0.7 }}>
-                        <img src="/search.svg"></img>
-
-                    </div>
+                    <img src="/brand.svg"></img>
                     <Flex>
                         <AskAIButtonSC $backgroundColor="midnight"><img className="mb-1" src="/white-wand.svg"></img><span className="text">Ask AI</span></AskAIButtonSC>
                         <img src="/burger.svg" onClick={toggleSidebar} className="toggle-button"></img>
                         <span onClick={toggleSidebar} className={`close-button ${isOpen ? 'open' : ''} my-5`}>X</span>
                         <div className={`sidebar ${isOpen ? 'open' : ''}`}>
                             <Header></Header>
+                            <div className="pl-5">
+                                <SectionTitle>New Investment</SectionTitle>
+                                <AddInvestmentContainer></AddInvestmentContainer>
+                            </div>
+                            <div className="pl-5">
+                                <AIQueryButtonContainer></AIQueryButtonContainer>
+                            </div>
+                            <div>
+                                <ConfigurationButtonContainer></ConfigurationButtonContainer>
+                            </div>
                         </div>
                     </Flex>
                 </Flex>
