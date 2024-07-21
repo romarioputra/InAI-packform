6c0fc16285bad45cc6afc53ff4176f855fe458f0
Romario <yohanes.romario@dealpos.com>
2024-07-21 17:23:32 +0700

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { AskAIButtonSC } from "./AskAIButton";
 import { Flex } from "./FlexCointainer";
+import "./Navbar.css"
 import Header from "./Header";
 
-
-
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
     return (
         <>
             <div className="sm:hidden block ">
@@ -20,13 +25,17 @@ export default function Navbar() {
                         <img src="/avatar.svg"></img>
                         <img src="/brand.svg"></img>
                     </Flex>
-                    <div style={{flexGrow: 0.7}}>
+                    <div style={{ flexGrow: 0.7 }}>
                         <img src="/search.svg"></img>
 
                     </div>
                     <Flex>
                         <AskAIButtonSC $backgroundColor="midnight"><img className="mb-1" src="/white-wand.svg"></img><span className="text">Ask AI</span></AskAIButtonSC>
-                        <img src="/burger.svg"></img>
+                        <img src="/burger.svg" onClick={toggleSidebar} className="toggle-button"></img>
+                        <span onClick={toggleSidebar} className={`close-button ${isOpen ? 'open' : ''} my-5`}>X</span>
+                        <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+                            <Header></Header>
+                        </div>
                     </Flex>
                 </Flex>
             </div>
